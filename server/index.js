6fef03d050f7ecd8b046a0343910d69b402c95c5
manuelguirado <manu22.mgb@gmail.com>
2025-04-21 18:56:40 +0200

    refactor: update client and server for real-time chat functionality with Socket.IO

--- server/index.js
+++ server/index.js
@@ -6,14 +6,22 @@ const port = process.env.PORT || 3000;
 const app = express();
 const server = createServer(app);
 const io  = new Server(server)
-io.on('connection', () => {
+io.on('connection', (socket) => {
     console.log('a user connected');
+    //listen when a user is disconnected
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    //listen when a user sends a message
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
 });
     app.get('/', (req, res) => {
     res.sendFile(process.cwd() + '/client/index.html');
 }
 );
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 }
 ); 
