793e6bf8601af2ccaf00a47afd7190ec64d59057
manuelguirado <manu22.mgb@gmail.com>
2025-04-22 17:49:27 +0200

    fix: update database connection and message handling logic

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import {Server} from 'socket.io';
 import dotenv from 'dotenv';
 import { createClient } from '@libsql/client';
 import {createServer} from 'node:http';
+dotenv.config( {path: './server/.env' });
 const port = process.env.PORT || 3000;
 const app = express();
 const server = createServer(app);
@@ -11,23 +12,48 @@ const io  = new Server(server, {
     connectionStateRecovery: {},
 
     })
-dotenv.config();
-//create db connection
+ if (!process.env.DATABASE_TOKEN || !process.env.DATABASE_URL) {
+    console.error('DATABASE_URL is not set');
+    console.error('DATABASE_TOKEN is not set');
+    process.exit(1);
+ }
+ //create db connection
 const db = createClient({
-    url: process.env.DB_URL,
-    authToken: process.env.DB_TOKEN,
+    url: process.env.DATABASE_URL,
+    authToken: process.env.DATABASE_TOKEN
 });
+//create a table
+await db.execute(`
+    CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      content TEXT
+    );
+  `);
 io.on('connection', (socket) => {
     console.log('a user connected');
     //listen when a user is disconnected
     socket.on('disconnect', () => {
+
         console.log('user disconnected');
     });
     //listen when a user sends a message
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
+    socket.on('chat message', async (msg) => {
+        let result;
+        try {
+          console.log(msg)
+        
+          result = await db.execute({
+            sql: `INSERT INTO messages (content) VALUES (:content)`,
+            args: { content: msg }
+          });
+        } catch (e) {
+          console.error(e)
+          return
+        }
+      
+        io.emit('chat message', msg, result.lastInsertRowid.toString())
+      });
     });
-});
     app.get('/', (req, res) => {
     res.sendFile(process.cwd() + '/client/index.html');
 }
